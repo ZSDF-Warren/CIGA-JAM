ec222818bc906d03b4016d54b9fba44cec76a163
ZSDF-Warren <38743613+ZSDF-Warren@users.noreply.github.com>
2019-07-06 17:17:17 +0800

    player
    player

--- Obj_Player.cs
+++ Obj_Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obj_Player : MonoBehaviour
+public class Obj_Player : Obj_Char
 {
     public Vector2Int dir;
 
@@ -14,10 +14,51 @@ public class Obj_Player : MonoBehaviour
     {
         
     }
+    void move(Vector2Int d)
+    {
+        var des = d + _cube.Pos;
+        foreach (var item in MapMgr.Instance().CubeList)
+        {
+            if(item.Pos==des)
+            {
+                if(item.WalkAble)
+                {
+                    dir = d;
+                    _cube.ObjAbove = null;
+
+                    _cube = item;
+                    _cube.ObjAbove = this;
+
+                    return;
+                }
+                else
+                {
+                    //撞墙
+                }
+            }
+        }
+        return;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            move(new Vector2Int(1, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            move(new Vector2Int(-1, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            move(new Vector2Int(0, -1));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            move(new Vector2Int(0, 1));
+        }
+
     }
 }
