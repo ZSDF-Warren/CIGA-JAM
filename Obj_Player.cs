9e97666c07bfe4f4bdbbd302c907047195e480c4
ZSDF-Warren <38743613+ZSDF-Warren@users.noreply.github.com>
2019-07-06 20:36:07 +0800

    player

--- Obj_Player.cs
+++ Obj_Player.cs
@@ -6,42 +6,106 @@ public class Obj_Player : Obj_Char
 {
     public Vector2Int dir;
 
-    public int Cross(Vector2Int v1,Vector2Int v2)
+    public int Cross(Vector2Int v1, Vector2Int v2)
     {
         return v1.x * v2.y - v2.x * v1.y;
     }
 
+    public void zombieMove()
+    {
+        //遍历
+        {
+            //向前走
+            if (true)
+            {
+                move(dir);
+            }
+            //backword
+            else if (true)
+            {
+                move(new Vector2Int(-dir.x, -dir.y));
+            }
+            //right
+            if (true)
+            {
+                var d = dir;
+                if (dir == new Vector2Int(1, 0))
+                {
+                    d = new Vector2Int(0, -1);
+                }
+                if (dir == new Vector2Int(0, 1))
+                {
+                    d = new Vector2Int(1, 0);
+                }
+                if (dir == new Vector2Int(-1, 0))
+                {
+                    d = new Vector2Int(0, 1);
 
+                }
+                if (dir == new Vector2Int(0, -1))
+                {
+                    d = new Vector2Int(-1, 0);
+                }
+                move(d);
+            }
+            //left
+            if (true)
+            {
+                var d = dir;
+                if (dir == new Vector2Int(1, 0))
+                {
+                    d = new Vector2Int(0, 1);
+                }
+                if (dir == new Vector2Int(0, 1))
+                {
+                    d = new Vector2Int(-1, 0);
+                }
+                if (dir == new Vector2Int(-1, 0))
+                {
+                    d = new Vector2Int(0, -1);
+
+                }
+                if (dir == new Vector2Int(0, -1))
+                {
+                    d = new Vector2Int(1, 0);
+                }
+                move(d);
+            }
+        }
+
+        //
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-    void move(Vector2Int d)
+    public void move(Vector2Int d)
     {
         var des = d + _cube.Pos;
         foreach (var item in MapMgr.Instance.CubeList)
         {
-            if(item.Pos==des)
+            if (item.Pos == des)
             {
-                if(item.WalkAble)
+                if (item.WalkAble)
                 {
-                    if(dir==d)
+                    if (dir == d)
                     {
                         //前
                     }
-                    else if(dir==new Vector2Int(-d.x,-d.y))
+                    else if (dir == new Vector2Int(-d.x, -d.y))
                     {
                         //后
                     }
                     else
                     {
-                        if(Cross(dir, d)==1)
+                        if (Cross(dir, d) == 1)
                         {
                             //left
                         }
 
-                        else if(Cross(dir, d) == -1)
+                        else if (Cross(dir, d) == -1)
                         {
                             //right
                         }
@@ -50,7 +114,7 @@ public class Obj_Player : Obj_Char
                             //bug
                         }
                     }
-                    
+
                     dir = d;
                     _cube.ObjAbove = null;
 
@@ -58,7 +122,7 @@ public class Obj_Player : Obj_Char
                     _cube.ObjAbove = this;
                     this.transform.position = _cube.transform.position;
                     return;
-                    
+
                 }
                 else
                 {
--- WayPointHelper.cs
+++ WayPointHelper.cs
@@ -4,6 +4,73 @@ using UnityEngine;
 
 public class WayPointHelper : MonoBehaviour
 {
-    public Obj_Player mainchara;
+    public Obj_Player player;
 
+
+
+    public void ShowWay()
+    {
+        ////遍历
+        //{
+        //    //向前走
+        //    if (true)
+        //    {
+        //        player.move(player.dir);
+        //    }
+        //    //backword
+        //    else if (true)
+        //    {
+        //        player.move(new Vector2Int(-player.dir.x, -player.dir.y));
+        //    }
+        //    //right
+        //    if (true)
+        //    {
+        //        var d = player.dir;
+        //        if (player.dir == new Vector2Int(1, 0))
+        //        {
+        //            d = new Vector2Int(0, -1);
+        //        }
+        //        if (player.dir == new Vector2Int(0, 1))
+        //        {
+        //            d = new Vector2Int(1,0);
+        //        }
+        //        if (player.dir == new Vector2Int(-1, 0))
+        //        {
+        //            d = new Vector2Int(0,1);
+
+        //        }
+        //        if (player.dir == new Vector2Int(0, -1))
+        //        {
+        //            d = new Vector2Int(-1,0);
+        //        }
+        //        player.move(d);
+        //    }
+        //    //left
+        //    if (true)
+        //    {
+        //        var d = player.dir;
+        //        if (player.dir == new Vector2Int(1, 0))
+        //        {
+        //            d = new Vector2Int(0, 1);
+        //        }
+        //        if (player.dir == new Vector2Int(0, 1))
+        //        {
+        //            d = new Vector2Int(-1, 0);
+        //        }
+        //        if (player.dir == new Vector2Int(-1, 0))
+        //        {
+        //            d = new Vector2Int(0, -1);
+
+        //        }
+        //        if (player.dir == new Vector2Int(0, -1))
+        //        {
+        //            d = new Vector2Int(1, 0);
+        //        }
+        //        player.move(d);
+        //    }
+        //}
+
+        ////
+
+    }
 }
