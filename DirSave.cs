c3548130bd96a7427cf18b7ae6c2bf6990359a50
ljx719476856li <719476856@qq.com>
2019-07-06 18:53:31 +0800

    方向枚举
    方向枚举

---
+++ DirSave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirSave
+{
+    private static DirSave _instance;
+    public static DirSave Instance()
+    {
+        if (_instance == null)
+            _instance = new DirSave();
+        return _instance;
+    }
+
+    DirSave() { }
+
+    public enum EDIR
+    {
+        eNone = -1,
+        eUp,
+        eDown,
+        eLeft,
+        eRight,
+    }
+
+
+}
