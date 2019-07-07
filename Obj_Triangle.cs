64bbebdd8003da37a6163a1d71c6c874236c8b4a
ljx719476856li <719476856@qq.com>
2019-07-07 14:23:37 +0800

    三角+心心
    三角+心心

---
+++ Obj_Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_Triangle : Obj_Char
+{
+    [Header("一键切关")]
+    public bool tri;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.transform.name);
+        if (other.transform.tag == "Player" || other.transform.tag == "Zombie")
+        {
+            tri = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (tri)
+        {
+            tri = false;
+
+            ScenesManager.Instance().SceneLoad();
+            Debug.LogError("二周目开始");
+        }
+    }
+}
