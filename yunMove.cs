3b98157de2af419c3b4dc59fce694f05271eab00
ljx719476856li <719476856@qq.com>
2019-07-07 17:01:24 +0800

    123
    213

---
+++ yunMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yunMove : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
